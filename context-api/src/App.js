97cab702be658bb62bf8dc6f901649612ec7e543
sun202x <sun202x@gmail.com>
2020-05-16 19:17:55 +0900

    동적 import 수정

--- context-api/src/App.js
+++ context-api/src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter } from 'react-router-dom';
 
 function App() {
     const examples = [
@@ -23,51 +22,36 @@ function App() {
     ];
 
     const [currentIndex, setCurrentIndex] = useState(0);
-    const [Component, setComponent] = useState();
+    const [Component, setComponent] = useState({});
 
-    const movePrevious = () => {
-        const preIndex = Math.max(currentIndex - 1, 0);
-        setCurrentIndex(preIndex);
-    }
-
-    const moveNext = () => {
-        const nextIndex = Math.min(currentIndex + 1, examples.length - 1);
-        setCurrentIndex(nextIndex);
-    }
+    const moveFirst = () => moveExample(0);
+    const movePrevious = () => moveExample(currentIndex - 1);
+    const moveNext = () => moveExample(currentIndex + 1);
+    const moveLast = () => moveExample(examples.length - 1);
+    const moveExample = (index) => setCurrentIndex(Math.max(Math.min(index, examples.length - 1), 0));
 
     useEffect(() => {
         import(`./code/${examples[currentIndex]}`).then((module) => {
-            let instance;
-
-            if (!module.default.prototype) {
-                instance = () => {
-                    return {
-                        render: () => module.default()
-                    }
-                }
-            } else {
-                instance = new module.default();
-            }
-
-            setComponent(instance);
+            setComponent({ comp: module.default });
         });
     }, [currentIndex]);
 
     return (
-        <BrowserRouter>
-            <div style={{ width: '1000px', margin: '0 auto' }}>
-                <h3>실전 리액트 프로그래밍</h3>
-                <div style={{ padding: 20, border: '5px solid gray', margin: '0 auto', height: '600px' }}>
-                    <div style={{ borderBottom: '1px solid gray', marginBottom: 20 }}>
-                        예제 코드({examples[currentIndex]}) 실행결과
-                    </div>
-                    {Component && Component.render()}
+        <div style={{ width: '1000px', margin: '0 auto' }}>
+            <h3>실전 리액트 프로그래밍</h3>
+            <div style={{ padding: 20, border: '5px solid gray', margin: '0 auto', height: '600px' }}>
+                <div style={{ borderBottom: '1px solid gray', marginBottom: 20 }}>
+                    예제 코드({examples[currentIndex]}) 실행결과
                 </div>
 
-                <button onClick={movePrevious}>이전</button>
-                <button onClick={moveNext}>다음</button>
+                {Component.comp && (<Component.comp />)}
             </div>
-        </BrowserRouter>
+
+            <button onClick={moveFirst}>{`<<`}</button>
+            <button onClick={movePrevious}>{`<`}</button>
+            <button onClick={moveNext}>{`>`}</button>
+            <button onClick={moveLast}>{`>>`}</button>
+        </div>
     );
 }
 
